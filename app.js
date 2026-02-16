2fda7a78fdff9e15e78d12b4f40c028bd51c1c50
Br4mb0r4x <pauknerova.barbora@zak.spspb.cz>
2026-02-16 08:59:13 +0000

    zmena

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 
+const profilDiv = document.getElementById("profile");
+ 
 
-try{
-    fetch("profile.json");
-    
-}
-catch
-{
-    console.error("stala se chyba");
-}
-
+fetch("./profile.json")
+  .then(response => {
+    // kontrola
+    if (!response.ok) {
+      throw new Error("Nepodařilo se načíst profile.json");
+    }
+    return response.json();
+  })
+  .then(data => {
+    profilDiv.innerHTML = `
+      <h2>${data.jmeno}</h2>
+ 
+      <p><strong>Zájmy:</strong></p>
+      <ul>
+        ${data.zajmy.map(zajem => `<li>${zajem}</li>`).join("")}
+      </ul>
+ 
+      <p><strong>Dovednosti:</strong></p>
+      <ul>
+        ${data.dovednosti.map(dovednost => `<li>${dovednost}</li>`).join("")}
+      </ul>
+    `;
+  })
+  .catch(err => {
+    console.error("Chyba:", err);
+    profilDiv.innerHTML = `
+      <p class="error">Profil nenacten</p>
+      <p>Zkus zkontrolovat, jestli soubor profil.json existuje ve stejné složce.</p>
+    `;
+  });
